c4789598d0ab3450b7637affa75edf90be12b2c2
mibrgmv <mibrgmv@gmail.com>
2024-06-13 10:30:05 +0300

    added javadoc to day 13

--- src/day13/Solution.java
+++ src/day13/Solution.java
@@ -3,6 +3,12 @@ package day13;
 import java.util.Arrays;
 
 class Solution {
+    /**
+     * day 13
+     * 2037. Minimum Number of Moves to Seat Everyone (Easy)
+     * Runtime: 97.35%
+     * Memory: 80.64%
+     */
     public int minMovesToSeat(int[] seats, int[] students) {
         int numberOfMoves = 0;
         Arrays.sort(seats);
