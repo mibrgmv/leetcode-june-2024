a6e72707fb842790348fd76503bc476717f3e9eb
mibrgmv <mibrgmv@gmail.com>
2024-06-16 22:02:54 +0300

    day 16

---
+++ src/day16/Solution.java
@@ -0,0 +1,26 @@
+package day16;
+
+public class Solution {
+    /**
+     * day 16
+     * 330. Patching Array
+     * Runtime: 100.00%
+     * Memory: 90.19%
+     */
+    public int minPatches(int[] nums, int n) {
+        int numOfPatches = 0;
+        long elem = 1;
+
+        for (int i = 0; elem <= n; ) {
+            if (i < nums.length && nums[i] <= elem) {
+                elem += nums[i];
+                ++i;
+            } else {
+                elem *= 2;
+                ++numOfPatches;
+            }
+        }
+
+        return numOfPatches;
+    }
+}
